721aeda4c06ff7f5f58e3e7c5f8d7a5d1ac47673
Nick Sette <njs2394@gmail.com>
2023-11-14 00:53:51 -0500

    ingredients

--- src/index.js
+++ src/index.js
@@ -1,22 +1,67 @@
 //1. Statz's code: Index.js : Rendering the the sandwiches at the top of the page. 
 //1. Fetch call to the db.json and parsing out all of the data.
 
+//2. Nick's Code: Index.js: Rendering the ingredients through click events 
 
 //1.1 Getting ID's to render the images
-document.addEventListener('DOMContentLoaded', () => {
-    const sandwichMenu = document.getElementById('menu')
+const sandwichMenu = document.getElementById('menu')
+const ingredientsList = document.getElementById('ingredients')
 
-    //1.2 Function to create images
-    function createSandwichImage(sandwich) {
-        const img = document.createElement('img')
-        img.src = sandwich.url
-        img.classList.add('sandwich-image') //Add a CSS class to the img element
-        sandwichMenu.appendChild(img) //Append the created img element to the sandwichMenu div
-    }
 
-    //1.3 Make an HTTP GET request to fetch the sandwich data from the server
-    fetch('http://localhost:3000/sandwiches')
-    .then((res) => res.json ())
+//1.2 Function to create images
+function createSandwichImage(sandwich) {
+    const img = document.createElement('img')
+    img.src = sandwich.url
+    img.classList.add('sandwich-image') //Add a CSS class to the img element
+    img.setAttribute('data-id', sandwich.id); // Set the data-id attribute to store the sandwich id
+    sandwichMenu.appendChild(img) //Append the created img element to the sandwichMenu div
+}
+
+sandwichMenu.addEventListener('click', (e) => {
+    const clickedImg = e.target;
+    const sandwichId = clickedImg.getAttribute('data-id'); // Get the value of the data-id attribute
+    console.log(sandwichId);
+    ingredientsList.innerHTML = ""
+    fetch(`http://localhost:3000/sandwiches/${sandwichId}`)
+        .then(res => res.json())
+        .then(data => {
+            let ingredientsIds = data['sandwich-ingredients-ids']
+            ingredientsIds.forEach(id => {
+                fetch(`http://localhost:3000/ingredients/${id}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        renderSandwichIngredientList(data)
+                    })
+            })
+        })
+
+})
+
+function renderSandwichIngredientList(ingredients) {
+
+    console.log(ingredients)
+    const li = document.createElement('li')
+    ingredientsList.append(li)
+
+    const p = document.createElement('p')
+    p.innerHTML = ingredients.name
+
+    const img = document.createElement('img')
+    img.src = ingredients.url
+    img.classList.add('ingredient-image') //Add a CSS class to the img element
+
+
+    li.appendChild(img)
+    li.appendChild(p)
+
+
+
+}
+
+
+//1.3 Make an HTTP GET request to fetch the sandwich data from the server
+fetch('http://localhost:3000/sandwiches')
+    .then((res) => res.json())
     .then(data => {
 
         //1.4 Iterate through the sandwich objects and create img tags for each
@@ -27,4 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch(error => {
         console.error('Error fetching data:', error)
     })
-})
+
+
+
+
